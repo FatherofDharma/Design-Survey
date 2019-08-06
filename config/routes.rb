15543918e8b18fb0ddbec066d979aca7d76d2b08
Daniel Hongel and Jess Doss <jess@>
2019-08-06 15:33:54 -0700

    add full views and crud functionality, with database backup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'surveys#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :surveys do
+    resources :questions
+  end
+
 end
